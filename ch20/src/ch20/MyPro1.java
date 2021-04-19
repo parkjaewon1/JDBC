1862bdc71e99037d56ec9392d1a82a19e7d04928
parkjaewon1 <48577510+parkjaewon1@users.noreply.github.com>
2021-04-19 14:07:34 +0900

    2021-04-19

---
+++ ch20/src/ch20/MyPro1.java
@@ -0,0 +1,33 @@
+package ch20;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+public class MyPro1 {
+	public static void main(String[] args) throws SQLException {
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String url = "jdbc:mysql://127.0.0.1:3306/test?useSSL=false&serverTimezone=UTC";
+		Connection conn = null;  CallableStatement cs = null;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("부서코드 ?");
+		int deptno = Integer.parseInt(sc.nextLine());
+		System.out.println("부서명 ?");
+		String dname = sc.nextLine();
+		System.out.println("근무지 ?");
+		String loc = sc.nextLine();
+		String sql = "{call dept_insert(?,?,?)}";
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, "root", "wo1041819");
+			cs   = conn.prepareCall(sql);
+			cs.setInt(1, deptno);  cs.setString(2, dname); cs.setString(3, loc);
+			int result = cs.executeUpdate();
+			if (result > 0) System.out.println("입력 성공");
+			else System.out.println("입력 실패");
+		}catch (Exception e) { System.out.println("실패 : "+e.getMessage());			
+		}finally { cs.close(); conn.close(); }
+		sc.close();
+	}
+}
